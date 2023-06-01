19eebc2ebf3c918047ce693b0560c4354375eb37
Lucas Nunes <lucas.nunes@hotmart.com>
2023-06-01 00:01:29 -0300

    atualizando footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="footer text-lg text-white">
+  <div class="footer text-lg text-white flex flex-col">
+    <img src="@/assets/images/line-vertical.svg" class="w-full mx-auto"/>
     <div class="flex lg:w-10/12 w-full justify-evenly align-center mx-auto">
       <div class="flex flex-col">
         <p v-for="(menu, index) in useMenu().value" :key="index">
@@ -24,24 +25,22 @@
     </div>
   </div>
 </template>
-<script>
-export default {
-  
-}
-</script>
-
 <style>
 .footer {
   padding: 2rem 0;
   margin: 0 auto;
   max-width: 90vw;
-  border-top: 1px solid white;
 }
 
-.footer hr {
+.footer::after {
+  content: '';
+  display: block;
+  position: absolute;
   width: 100%;
-  height: 2px;
-  border: 1px solid white;
-  border-radius: 55rem;
+  height: .6rem;
+  bottom: 0;
+  left: 0;
+  background: rgb(0,0,255);
+  background: linear-gradient(90deg, rgba(0,0,255,1) 0%, rgba(145,13,255,1) 30%);
 }
 </style>
